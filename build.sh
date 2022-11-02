8efe2d9a70fdc153f352dff0f0ee1360aa40f5e6
Maung Bodas <116504869+maung-bodaz@users.noreply.github.com>
2022-11-02 18:31:45 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yanzszs/local_manifest --depth 1 -b rice-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 10.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/maung-bodaz/local_manifest --depth 1 -b AEX-10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_whyred-userdebug
+lunch aosp_whyred-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=android-build
@@ -15,5 +15,5 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
-mka bacon -j8
+mka aex -j8
 # build

e8be82bcfa56cf4bc84afbf005b794d019d9bb8e
Maung Bodas <116504869+maung-bodaz@users.noreply.github.com>
2022-11-04 10:50:19 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b ten -g default,-mips,-darwin,-notdefault
-git clone https://github.com/maung-bodaz/local_manifest --depth 1 -b AEX-10 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b banana-13-ginkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_whyred-userdebug
+lunch banana_ginkgo-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=android-build
@@ -16,4 +16,4 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
 mka bacon -j8
-# build
+# build 1

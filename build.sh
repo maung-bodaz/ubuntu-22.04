f6470a2f6a3dc00801695f585f0445efbaed8a6a
Maung Bodas <116504869+maung-bodaz@users.noreply.github.com>
2022-12-11 06:49:56 +0700

    Test

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#sync rom
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b ten -g default,-mips,-darwin,-notdefault
 git clone https://github.com/maung-bodaz/local_manifest --depth 1 -b AEX-10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
